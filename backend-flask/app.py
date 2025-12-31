cfc100fbe6740d4cbf30546f824df9694c3d32d3
GT <gtnagarajlpu@gmail.com>
2025-12-31 18:14:47 +0530

    Addinng OTEL python imports, Creating the tracer and Adding Automatic FlaskInstrumentatiion

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -13,7 +13,30 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+# OTEL Python SDK Imports
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+# implementing a provider and exporting traces
+
+provider= TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+
+
+
 app = Flask(__name__)
+
+# OpenTelemetry Configuration
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
